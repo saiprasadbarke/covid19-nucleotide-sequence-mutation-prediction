954e0d831761ad6c415ff95479cec02a8cae7f1f
saiprasad_barke <barke.saiprasad1992@gmail.com>
2022-03-28 11:22:51 +0530

    func to create subset fasta file

--- data_preprocessing/fasta_file_operations.py
+++ data_preprocessing/fasta_file_operations.py
@@ -1,17 +1,23 @@
 # Standard
-
+from pathlib import Path
 # Local
-
+from data_preprocessing.helper import batch_iterator
 # External
 from Bio import SeqIO
 
-
+#DATA_FOLDER = Path("")
 def read_fasta_file(import_file_path: str):  # provide return types here
-    for seq_record in SeqIO.parse("data/spikenuc0312.fasta", "fasta"):
+
     return None
 
 
-def create_subset_fasta_file(export_file_path: str):
+def create_subset_fasta_file(import_file_path: str, export_file_path: str = None):
+    record_iter = SeqIO.parse(open(import_file_path), "fasta")
+    for i, batch in enumerate(batch_iterator(record_iter, 25000)):
+        filename = "group_%i.fasta" % (i + 1)
+        with open(filename, "w") as handle:
+            count = SeqIO.write(batch, handle, "fasta")
+        print("Wrote %i records to %s" % (count, filename))
     return None
 
 
@@ -20,6 +26,11 @@ def count_number_of_sequences(import_file_path: str):
     sequence_file = open(import_file_path)
     for sequence in sequence_file:
         if sequence.startswith(">"):
-            count+=1
+            count += 1
     sequence_file.close()
     return count
+
+
+if __name__ == "__main__":
+    fasta_file_path = "spikenuc0312.fasta"
+    create_subset_fasta_file(import_file_path=fasta_file_path)
---
+++ data_preprocessing/helper.py
@@ -0,0 +1,28 @@
+def batch_iterator(iterator, batch_size):
+    """Returns lists of length batch_size.
+
+    This can be used on any iterator, for example to batch up
+    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+    Alignment objects from Bio.AlignIO.parse(...), or simply
+    lines from a file handle.
+
+    This is a generator function, and it returns lists of the
+    entries from the supplied iterator.  Each list will have
+    batch_size entries, although the final list may be shorter.
+
+    Ref: https://biopython.org/wiki/Split_large_file
+    """
+    entry = True  # Make sure we loop once
+    while entry:
+        batch = []
+        while len(batch) < batch_size:
+            try:
+                entry = iterator.next()
+            except StopIteration:
+                entry = None
+            if entry is None:
+                # End of file
+                break
+            batch.append(entry)
+        if batch:
+            yield batch
