23349661a2fa80cdf361ce8d1cfe315ecb62c326
saiprasad_barke <barke.saiprasad1992@gmail.com>
2022-03-28 09:38:37 +0530

    Added func to count number of sequences in a file

--- data_preprocessing/fasta_file_operations.py
+++ data_preprocessing/fasta_file_operations.py
@@ -6,9 +6,20 @@
 from Bio import SeqIO
 
 
-def read_fasta_file(file_path: str):  # provide return types here
+def read_fasta_file(import_file_path: str):  # provide return types here
+    for seq_record in SeqIO.parse("data/spikenuc0312.fasta", "fasta"):
     return None
 
 
+def create_subset_fasta_file(export_file_path: str):
+    return None
 
 
+def count_number_of_sequences(import_file_path: str):
+    count = 0
+    sequence_file = open(import_file_path)
+    for sequence in sequence_file:
+        if sequence.startswith(">"):
+            count+=1
+    sequence_file.close()
+    return count
