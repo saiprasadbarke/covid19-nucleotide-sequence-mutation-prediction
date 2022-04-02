eedc62429d9dce6187f7cd840fd5e1afb89e67ec
saiprasad_barke <barke.saiprasad1992@gmail.com>
2022-04-02 11:33:31 +0530

    Added stats dump

--- visualization/plot_by_clades.py
+++ visualization/plot_by_clades.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
+import json
 
 
 def read_clade_tabular(file_path: str) -> dict[str, str]:
@@ -13,7 +14,7 @@ def read_clade_tabular(file_path: str) -> dict[str, str]:
     return id_clade_dict
 
 
-def plot_bar_clades(id_clade_dict: dict[str, str], op_path: str):
+def plot_bar_clades(id_clade_dict: dict[str, str], graph_path: str, stats_path: str):
     clades = list(id_clade_dict.values())
     unique_clades = []
     for clade in clades:
@@ -22,21 +23,34 @@ def plot_bar_clades(id_clade_dict: dict[str, str], op_path: str):
     number_of_clades_dict = dict.fromkeys(unique_clades)
     for unique_clade in unique_clades:
         number_of_clades_dict[unique_clade] = clades.count(unique_clade)
-    x = [str(clade) for clade in list(number_of_clades_dict.keys())]
+    number_of_clades_dict = dict(
+        sorted(number_of_clades_dict.items(), key=lambda item: item[1])
+    )
+    with open(stats_path, "w") as fp:
+        json.dump(number_of_clades_dict, fp)
+    print(number_of_clades_dict)
+    x = [
+        str(clade) for clade in list(number_of_clades_dict.keys())
+    ]  # Need to perform this step as x has some nan values
     y = list(number_of_clades_dict.values())
-    plt.xlabel("Clades")
-    plt.title("Frequency of clades")
+
     plt.figure(figsize=(20, 20))
-    plt.xticks(rotation=90)
     plt.bar(x, y)
-    plt.savefig(op_path)
+    plt.xlabel("Clades")
+    plt.xticks(rotation=90)
+    plt.title("Frequency of clades")
+    plt.savefig(graph_path)
     plt.show()
 
 
 if __name__ == "__main__":
-    clade_filepath = (
-        f"{Path.cwd()}/data/countrywise_split/clades_tabular/india_clades.tabular"
-    )
-    clades_histogram_path = f"{Path.cwd()}/plots/clades_india_hist.png"
+    clade_filepath = f"{Path.cwd()}/data/complete_sequences/complete_clades.tabular"
+    plot_name = "clades_all_hist"
+    clades_histogram_path = f"{Path.cwd()}/plots/{plot_name}.png"
     id_clade_dict = read_clade_tabular(clade_filepath)
-    plot_bar_clades(id_clade_dict=id_clade_dict, op_path=clades_histogram_path)
+    stats_file = f"{Path.cwd()}/plots/stats/{plot_name}.json"
+    plot_bar_clades(
+        id_clade_dict=id_clade_dict,
+        graph_path=clades_histogram_path,
+        stats_path=stats_file,
+    )
