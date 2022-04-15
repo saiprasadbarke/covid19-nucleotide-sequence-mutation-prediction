fd69bbe8e3d778e4d26341c690d8ec2e5181b57e
saiprasad_barke <barke.saiprasad1992@gmail.com>
2022-04-15 14:30:48 +0530

    added regex for cleaning sequences

--- src/data_operations/fasta_file_operations.py
+++ src/data_operations/fasta_file_operations.py
@@ -12,24 +12,22 @@ def create_cleaned_fasta_file(import_file_path: str, export_file_path: str):
     fasta_file_with_cleaned_sequences = open(export_file_path, "w")
     number_of_clean_sequences = 0
     for seq_record in SeqIO.parse(import_file_path, "fasta"):
-        if check_sequence_completeness(sequence=str(seq_record.seq)):
+        if is_valid_sequence(sequence=str(seq_record.seq)):
             number_of_clean_sequences += 1
             fasta_file_with_cleaned_sequences.write(seq_record.format("fasta"))
             if number_of_clean_sequences % 100000 == 0:
                 print(f"Wrote {number_of_clean_sequences} clean sequences to file")
+    print(f"Wrote {number_of_clean_sequences} clean sequences to file")
     fasta_file_with_cleaned_sequences.close()
 
 
-def check_sequence_completeness(sequence: str) -> bool:
-    if "N" in sequence or "." in sequence or "-" in sequence:
-        return False
-    else:
-        return True
+def is_valid_sequence(sequence: str) -> bool:
+    return match(r"^([ACTG])+$", sequence)
 
 
 if __name__ == "__main__":
     input_fasta_file_path = f"{Path.cwd()}/data/00unfiltered/unfiltered.fasta"
-    sequences_file_path = f"{Path.cwd()}/data/01cleaned/sequences.fasta"
+    sequences_file_path = f"{Path.cwd()}/data/01cleaned/sequences_new.fasta"
     create_cleaned_fasta_file(
         import_file_path=input_fasta_file_path,
         export_file_path=sequences_file_path,
