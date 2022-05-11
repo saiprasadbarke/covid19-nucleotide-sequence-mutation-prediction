5ae82e8ea97804f51f195f24e8e63cd8cb49dfc1
saiprasadbarke <barke.saiprasad1992@gmail.com>
2022-05-11 12:48:20 +0000

    Added training components

---
+++ src/model_training/batch.py
@@ -0,0 +1,38 @@
+from settings.constants import USE_CUDA
+
+class Batch:
+    """Object for holding a batch of data with mask during training.
+    Input is a batch from a pytorch dataloader.
+    """
+    def __init__(self, src, trg, pad_index=0):
+        
+        src, src_lengths = src
+        
+        self.src = src
+        self.src_lengths = src_lengths
+        self.src_mask = (src != pad_index).unsqueeze(-2)
+        self.nseqs = src.size(0)
+        
+        self.trg = None
+        self.trg_y = None
+        self.trg_mask = None
+        self.trg_lengths = None
+        self.ntokens = None
+
+        if trg is not None:
+            trg, trg_lengths = trg
+            self.trg = trg[:, :-1]
+            self.trg_lengths = trg_lengths
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = (self.trg_y != pad_index)
+            self.ntokens = (self.trg_y != pad_index).data.sum().item()
+        
+        if USE_CUDA:
+            self.src = self.src.cuda()
+            self.src_mask = self.src_mask.cuda()
+
+            if trg is not None:
+                self.trg = self.trg.cuda()
+                self.trg_y = self.trg_y.cuda()
+                self.trg_mask = self.trg_mask.cuda()
+                
