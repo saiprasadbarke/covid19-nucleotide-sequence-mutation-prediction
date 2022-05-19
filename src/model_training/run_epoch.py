e9372936932fb2d37f2de79154973c2ed424d679
saiprasadbarke <barke.saiprasad1992@gmail.com>
2022-05-19 12:32:04 +0000

    changed run_epoch to remove mask

--- src/model_training/run_epoch.py
+++ src/model_training/run_epoch.py
@@ -12,23 +12,23 @@ def run_epoch(data_iter, model: EncoderDecoder, loss_compute, print_every=50):
     print_tokens = 0
 
     for i, batch in enumerate(data_iter, 1):
-
+        src, trg = batch[0], batch[1]
         out, _, pre_output = model.forward(
-            batch[0],
-            batch[1],
+            src,
+            trg,
             # batch.src_mask,
             # batch.trg_mask,
             # batch.src_lengths,
             # batch.trg_lengths,
         )
-        loss = loss_compute(pre_output, batch.trg_y, batch.nseqs)
+        loss = loss_compute(pre_output, trg[:, 1:], src.size(0))
         total_loss += loss
-        total_tokens += batch.ntokens
-        print_tokens += batch.ntokens
+        total_tokens += trg[:, 1:].data.sum().item()
+        print_tokens += trg[:, 1:].data.sum().item()
 
         if model.training and i % print_every == 0:
             elapsed = time.time() - start
-            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" % (i, loss / batch.nseqs, print_tokens / elapsed))
+            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" % (i, loss / src.size(0), print_tokens / elapsed))
             start = time.time()
             print_tokens = 0
 
