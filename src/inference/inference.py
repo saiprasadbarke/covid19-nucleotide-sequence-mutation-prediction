3fa45f62202a250a9331b97f92423d4da0bdfe65
saiprasad_barke <barke.saiprasad1992@gmail.com>
2022-06-08 12:28:08 +0200

    Changed inferece to compute diff wrt ref genome

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,3 +1,4 @@
+from json import dump
 from pathlib import Path
 from statistics import mean
 from typing import List
@@ -5,6 +6,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from data_operations.train_val_test_split import get_split_data
+from data_operations.vocabulary import Vocabulary
 from inference.greedy_search import greedy_decode
 from metrics.index_diff import index_diff
 from model_components.create_model import create_model
@@ -20,25 +22,44 @@ from settings.constants import (
     RUN_NAME,
     SAVED_MODELS_PATH,
 )
+from settings.reference_sequence import WUHAN_REF
+from data_operations.search_space_minimization import get_string_difference_indices
+from visualization.plot_mutation_sites import plot_mutations
 
 
 def test_model(test_dataloader: DataLoader, model: EncoderDecoder):
+    vocab = Vocabulary(3)
     ground_truth_sequences = []
     predicted_sequences = []
     alphas = []
-    index_mismatch = []
+    difference_indices_wuhan = {}
     for batch in test_dataloader:
         batch = rebatch(batch)
         ground_truth = batch.trg_y[:, :-1].tolist()[0]
         ground_truth_sequences.append(ground_truth)
         pred, attention = greedy_decode(model, batch.src_input, max_len=499)
-        predicted_sequences.append(pred)
-        alphas.append(attention)
-        index_mismatch.append(index_diff(ground_truth, pred))
-    average_mismatch = mean(index_mismatch)
-    min_mismatch = min(index_mismatch)
-    max_mismatch = max(index_mismatch)
-    return ground_truth_sequences, predicted_sequences, alphas, index_mismatch
+        lol = [vocab.itos[idx] for idx in pred]
+        concat = []
+        for index, kmer in enumerate(lol, 1):
+            if index != 498:
+                concat.append(kmer[0])
+            else:
+                concat.append(kmer)
+        concat = "".join(concat)
+        difference_wuhan = get_string_difference_indices(WUHAN_REF[0:499], concat)
+        for index in difference_wuhan:
+            if index in difference_indices_wuhan.keys():
+                difference_indices_wuhan[index] += 1
+            else:
+                difference_indices_wuhan[index] = 1
+        predicted_sequences.append(concat)
+    difference_indices_file_wuhan = f"{SAVED_MODELS_PATH}/{RUN_NAME}/difference_indices_wuhan.json"
+    with open(difference_indices_file_wuhan, "w") as fout:
+        dump(difference_indices_wuhan, fout)
+    mutations_graph_path_wuhan = f"{SAVED_MODELS_PATH}/{RUN_NAME}/mutation_sites_wuhan.png"
+    data_dump_path_wuhan = f"{SAVED_MODELS_PATH}/{RUN_NAME}/sorted_difference_indices_wuhan.json"
+    plot_mutations(difference_indices_file_wuhan, mutations_graph_path_wuhan, data_dump_path_wuhan)
+    return predicted_sequences
 
 
 def plot_mismatch(mismatch_list: int, graph_path: str):
@@ -62,6 +83,5 @@ def inference():
     )
     checkpoint = torch.load(f"{SAVED_MODELS_PATH}/{RUN_NAME}/MODEL_{35}.pt")
     model.load_state_dict(checkpoint["model_state_dict"])
-    ground_truth_sequences, predicted_sequences, alphas, mismatch_list = test_model(test_dataloader, model)
-    graph_path = f"{Path.cwd()}/reports/plots/index_mismatch_post_prediction.png"
-    plot_mismatch(mismatch_list, graph_path)
+    test_model(test_dataloader, model)
+
